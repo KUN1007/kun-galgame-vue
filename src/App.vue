1d89f7e50cb0c7524a8f4668ae0ed9d275006a67
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-04-14 21:58:13 +0800

    Create KUNGalgame Vue version

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script lang="ts">
+import KUNGalgameMainPage from "./views/Home/index.vue";
+export default {
+  name: "App",
+  components: { KUNGalgameMainPage },
+};
+</script>
+
+<template>
+  <KUNGalgameMainPage />
+</template>
+
+<style type="scss" scoped></style>
---
+++ src/views/Home/index.vue
@@ -0,0 +1,31 @@
+<script lang="ts">
+import KUNGalgameTopBar from "../../components/KUNGalgameTopBar.vue";
+
+export default {
+  name: "KUNGalgameMainPage",
+  components: { KUNGalgameTopBar },
+};
+</script>
+
+<template>
+  <!-- 可视部分的容器 -->
+  <div class="visual-wrapper">
+    <KUNGalgameTopBar />
+  </div>
+</template>
+
+<style lang="less" scoped>
+.visual-wrapper {
+  height: 1500px;
+  display: flex;
+  flex-direction: column;
+  // background-image: url(../../assets/images/bg.png);
+  // background-image: url(./img/bg-dark.png);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
+  min-width: 700px;
+  font-family: "adobe-clean", "Source Sans Pro", -apple-system,
+    "BlinkMacSystemFont", "Segoe UI,Roboto", sans-serif;
+}
+</style>
