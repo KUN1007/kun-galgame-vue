81fcddd7c6c5d3e01ad93ac3b3ccbf18b63c5993
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-04-17 23:20:49 +0800

    import color

--- src/components/KUNGalgameTopBar.vue
+++ src/components/KUNGalgameTopBar.vue
@@ -7,16 +7,6 @@ export default {
   },
   setup() {
     let topBarItem: string[] = ["回到主页", "帖子发布", "技术交流", "关于我们"];
-    /* 对象式
-    
-        let topBarItem: object[] = [
-      { id: "001", name: "回到主页" },
-      { id: "002", name: "帖子发布" },
-      { id: "003", name: "技术交流" },
-      { id: "004", name: "关于我们" },
-    ];
-    
-    */
     return {
       topBarItem,
     };
@@ -53,145 +43,111 @@ export default {
 </template>
 
 <style lang="less" scoped>
-// 网站名字颜色
-@kungalgame: #3a3a3a;
-// 导航栏的背景色
-@kungalgame-header-background: #ffffff80;
-// 导航栏的阴影颜色
-@kungalgame-header-shadow: #008cff2b;
-// 导航栏的文字颜色
-@kungalgame-blue: #0969da;
-// 导航栏的文字 hover
-@kungalgame-nav-hover: #ecf3ff;
-// 导航栏的文字块 hover
-@kungalgame-hover-blue: #409eff;
-@kungalgame-hover-red: #f56c6c;
-@kungalgame-hover-yellow: #ffb900;
-@kungalgame-hover-green: #67c23a;
-// 用户名颜色
-@kungalgamer: #171717;
-
-/* 头部样式 */
 .header {
-  height: 58px;
-  /* 头部下方阴影 */
-  box-shadow: 0 2px 4px 0 @kungalgame-header-shadow;
-  /* 头部背景 */
-  backdrop-filter: blur(5px);
-  background-color: @kungalgame-header-background;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-right: 50px;
+	height: 58px;
+	box-shadow: 0 2px 4px 0 var(--kungalgame-trans-blue-1);
+	backdrop-filter: blur(5px);
+	background-color: var(--kungalgame-trans-white-5);
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-right: 50px;
 }
 .nav-top {
-  display: flex;
-  align-items: center;
+	display: flex;
+	align-items: center;
 }
 .kungal-info {
-  display: flex;
-  align-items: center;
-  // 网站 LOGO
-  img {
-    height: 50px;
-    margin-left: 50px;
-  }
-  // 网站名称
-  span {
-    margin-left: 20px;
-    margin-right: 7px;
-    color: @kungalgame;
-    font-weight: bold;
-  }
+	display: flex;
+	align-items: center;
+	img {
+		height: 50px;
+		margin-left: 50px;
+		cursor: pointer;
+	}
+	span {
+		margin-left: 20px;
+		margin-right: 7px;
+		color: var(--kungalgame-font-color-3);
+		font-weight: bold;
+		cursor: pointer;
+	}
 }
-/* 顶部导航栏 */
 .top-bar {
-  width: 400px;
-  position: relative;
-  text-align: center;
-  ul {
-    display: flex;
-    align-items: center;
-    right: 5%;
-    .top-bar-box {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      width: calc((100% / 4) * 1);
-      height: 7px;
-      border-radius: 2px;
-      transition: 0.5s;
-    }
-    li {
-      width: 100%;
-      color: @kungalgame-blue;
-      font-weight: bold;
-      display: block;
-      width: 100%;
-      line-height: 58px;
-      cursor: pointer;
-      &:hover {
-        background-color: @kungalgame-nav-hover;
-        transition: 0.5s;
-        border-radius: 2px;
-      }
-      &:nth-child(1) {
-        &:hover {
-          & ~ .top-bar-box {
-            left: calc(100% / 4 * 0);
-            background-color: @kungalgame-hover-red;
-          }
-        }
-      }
-      &:nth-child(2) {
-        &:hover {
-          & ~ .top-bar-box {
-            left: calc(100% / 4 * 1);
-            background-color: @kungalgame-hover-yellow;
-          }
-        }
-      }
-      &:nth-child(3) {
-        &:hover {
-          & ~ .top-bar-box {
-            left: calc(100% / 4 * 2);
-            background-color: @kungalgame-hover-blue;
-          }
-        }
-      }
-      &:nth-child(4) {
-        &:hover {
-          & ~ .top-bar-box {
-            left: calc(100% / 4 * 3);
-            background-color: @kungalgame-hover-green;
-          }
-        }
-      }
-    }
-  }
+	width: 400px;
+	position: relative;
+	text-align: center;
+	ul {
+		display: flex;
+		align-items: center;
+		right: 5%;
+		.top-bar-box {
+			position: absolute;
+			bottom: 0;
+			left: 0;
+			width: calc((100% / 4) * 1);
+			height: 7px;
+			border-radius: 2px;
+			transition: 0.5s;
+		}
+		li {
+			width: 100%;
+			color: var(--kungalgame-blue-5);
+			font-weight: bold;
+			display: block;
+			width: 100%;
+			line-height: 58px;
+			cursor: pointer;
+			&:hover {
+				background-color: var(--kungalgame-blue-0);
+				transition: 0.5s;
+				border-radius: 2px;
+			}
+			&:nth-child(1) {
+				&:hover {
+					&~.top-bar-box {
+						left: calc(100% / 4 * 0);
+						background-color: var(--kungalgame-red-3);
+					}
+				}
+			}
+			&:nth-child(2) {
+				&:hover {
+					&~.top-bar-box {
+						left: calc(100% / 4 * 1);
+						background-color: var(--kungalgame-yellow-3);
+					}
+				}
+			}
+			&:nth-child(3) {
+				&:hover {
+					&~.top-bar-box {
+						left: calc(100% / 4 * 2);
+						background-color: var(--kungalgame-blue-3);
+					}
+				}
+			}
+			&:nth-child(4) {
+				&:hover {
+					&~.top-bar-box {
+						left: calc(100% / 4 * 3);
+						background-color: var(--kungalgame-green-3);
+					}
+				}
+			}
+		}
+	}
 }
-
-/* 顶部搜索框 */
-/* 
-
-TODO:
-
-*/
-
-/* 用户个人信息 */
 .kungalgamer-info {
-  display: flex;
-  align-items: center;
-  // 用户头像
-  img {
-    border-radius: 50%;
-    height: 40px;
-  }
-  // 用户名
-  span {
-    font-weight: bold;
-    color: @kungalgamer;
-    margin-left: 20px;
-  }
+	display: flex;
+	align-items: center;
+	img {
+		border-radius: 50%;
+		height: 40px;
+	}
+	span {
+		color: var(--kungalgame-font-color-2);
+		margin-left: 20px;
+	}
 }
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,8 @@
+// core
 import { createApp } from 'vue'
 import './assets/css/reset.css'
 import App from './App.vue'
-
+// css
+import "./assets/css/reset.css"
+import "./styles/color.css"
 createApp(App).mount('#app')
