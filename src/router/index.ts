02efc54d898af58ef59c281bd7615a8eb94c66f8
KUN1007 <yuyu_kun@sina.com>
2023-04-20 16:04:08 +0800

    Add vue router

---
+++ src/router/index.ts
@@ -0,0 +1,41 @@
+import {
+  type RouteRecordRaw,
+  createRouter,
+  createWebHashHistory,
+  createWebHistory,
+} from "vue-router";
+
+/** 常驻路由 */
+export const constantRoutes: RouteRecordRaw[] = [
+  {
+    name:'topic',
+    path: "/topic",
+    component: () => import("@/views/topic/KUNGalgameTopicPage.vue")
+  },
+];
+
+const router = createRouter({
+  history:
+    import.meta.env.VITE_ROUTER_HISTORY === "hash"
+      ? createWebHashHistory(import.meta.env.VITE_PUBLIC_PATH)
+      : createWebHistory(import.meta.env.VITE_PUBLIC_PATH),
+  routes: constantRoutes,
+});
+
+/** 重置路由 */
+export function resetRouter() {
+  // 注意：所有动态路由路由必须带有 Name 属性，否则可能会不能完全重置干净
+  try {
+    router.getRoutes().forEach((route: { name: any; meta: any }) => {
+      const { name, meta } = route;
+      if (name && meta.roles?.length) {
+        router.hasRoute(name) && router.removeRoute(name);
+      }
+    });
+  } catch (error) {
+    // 强制刷新浏览器也行，只是交互体验不是很好
+    window.location.reload();
+  }
+}
+
+export default router;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
+import path from 'path'
 import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  /* src 别名为 @ */
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 });
