a3e418523e3c242d233eac5dab5cc9449aae2efd
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-05-21 00:16:15 +0800

    Complete lang switch

--- src/language/i18n.ts
+++ src/language/i18n.ts
@@ -1,13 +1,10 @@
 import { createI18n } from 'vue-i18n'
-// 获取网站 localStorage 中的默认语言
-import { getLangStatus } from '@/utils/cache/local-storage'
 // 引入语言文件
 import zh from './zh'
 import en from './en'
 
 const i18n = createI18n({
-  locale: getLangStatus() || 'en',
-  // locale: 'zh',
+  locale: localStorage.getItem('locale') || 'en',
   // 支持 Vue3 composition API
   legacy: false,
   // 全局注册 ts 方法
