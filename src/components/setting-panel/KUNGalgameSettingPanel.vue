efa523e51bc4065d2a23bb39b0b2dfae483953d9
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-05-22 23:42:21 +0800

    add mockjs

--- src/components/setting-panel/KUNGalgameSettingPanel.vue
+++ src/components/setting-panel/KUNGalgameSettingPanel.vue
@@ -14,7 +14,10 @@ import { storeToRefs } from 'pinia'
 // 导入 i18n
 import { useI18n } from 'vue-i18n'
 
-const { t, locale } = useI18n({ useScope: 'global' })
+/*
+ * 网站的语言设置
+ */
+const { locale } = useI18n({ useScope: 'global' })
 const selectedLocale = ref(locale.value)
 
 // 监听selectedLocale的变化，并更新Vue I18n的locale
@@ -24,7 +27,7 @@ watch(selectedLocale, (newVal) => {
 
 // 在页面加载时从localStorage中读取保存的语言设置
 onMounted(() => {
-  const savedLocale = localStorage.getItem('locale')
+  const savedLocale = localStorage.getItem('KUNGalgame-locale')
   if (savedLocale) {
     selectedLocale.value = savedLocale
   }
@@ -32,13 +35,16 @@ onMounted(() => {
 
 // 监听语言变化，并将语言设置保存到localStorage
 watch(locale, (newVal) => {
-  localStorage.setItem('locale', newVal)
+  localStorage.setItem('KUNGalgame-locale', newVal)
 })
 
 const changeLanguage = () => {
   locale.value = selectedLocale.value
 }
 
+/*
+ * 设置面板显示切换
+ */
 // 使用设置面板的 store
 const settingsStore = useSettingsPanelStore()
 const { showSettings } = storeToRefs(settingsStore)
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,12 @@ import path, { resolve } from 'path'
 import vue from '@vitejs/plugin-vue'
 // 引入 Rollup Plugin Visualizer 打包可视化工具
 import { visualizer } from 'rollup-plugin-visualizer'
+// 引入 mock-js
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), visualizer() as PluginOption],
+  plugins: [vue(), visualizer() as PluginOption, viteMockServe()],
   /* src 别名为 @ */
   resolve: {
     alias: {
