d7818d9d8c9043ef5ea0d2e5b337f563f32a2f3f
KUN1007 <yuyu_kun@sina.com>
2023-05-30 15:27:05 +0800

    fix topic comment

--- src/views/topic/content/others/components/Comments.vue
+++ src/views/topic/content/others/components/Comments.vue
@@ -7,13 +7,13 @@ import SingleComment from './SingleComment.vue'
 
 <template>
   <!-- 底部的评论 -->
-  <div class="bottom">
+  <div class="comment">
     <!-- 评论的标题 -->
-    <div class="comment-title">
+    <div class="title">
       <span>评论</span>
     </div>
     <!-- 评论容器 -->
-    <div class="comment-container">
+    <div class="container">
       <SingleComment />
       <SingleComment />
       <SingleComment />
@@ -23,18 +23,21 @@ import SingleComment from './SingleComment.vue'
 
 <style scoped>
 /* 底部的评论 */
-.bottom {
+.comment {
   display: flex;
   align-items: center;
+  justify-content: flex-start;
+  flex-shrink: 0;
   margin-top: 10px;
 }
 /* 评论的标题 */
-.comment-title {
-  margin: 0 auto;
+.title {
+  flex-shrink: 0;
+  margin: 10px;
 }
 /* 评论容器 */
-.comment-container {
-  width: 95%;
-  margin: 0 auto;
+.container {
+  width: 100%;
+  margin-bottom: 10px;
 }
 </style>
--- src/views/topic/content/others/components/SingleComment.vue
+++ src/views/topic/content/others/components/SingleComment.vue
@@ -1,7 +1,12 @@
 <!-- 
   这是单个评论的组件
  -->
-<script setup lang="ts"></script>
+<!-- 
+  ，对这些数据项的描述为：帖子序号（帖子的发布序号，唯一值， number 类型，从 1 开始排列），帖子楼层数（单个帖子在帖子页面的序列号，为 number），帖子内容，帖子发布时间（按时间戳返回），帖子热度值（一个 number 类型的数值），帖子标签（为一个 string 的数组），帖子被推数（number 类型的数字），帖子浏览数（number 类型的数字），帖子点赞数（number 类型的数字），帖子被踩数（number 类型的数字），帖子回复（类型是一个对象，里面的属性为：（单个回复，对帖子的单个回复，为一个对象，里面属性为：（回复人：是一个对象，里面的属性为（回复人的头像：是一张图片，回复人的名字：记录了回复人的名字，是一个 string，回复人的萌萌点：为一个数值），被回复的人：是一个 string，是否重新编辑：为一个 boolean，为真则返回）），单个回复的数量：为一个 number 数字，是对单个回复的计数）
+  -->
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+</script>
 
 <template>
   <div class="container">
@@ -10,27 +15,28 @@
     <!-- 单个评论的内容区 -->
     <div class="content">
       <!-- 单个评论内容区顶部 -->
-      <div class="single-comment-content-top">
+      <div class="describe">
         <!-- 顶部左侧名字 -->
-        <div class="single-comment-left">
-          鲲回复@<a href="#">啊这可海星啊这</a>
-        </div>
+        <div class="name">鲲评论 @<a href="#">啊这可海星啊这</a></div>
         <!-- 顶部右侧点赞、踩 -->
-        <div class="single-comment-right">
+        <div class="operate">
           <ul>
             <!-- 点赞 -->
             <li>
-              <a href="#"><i class="fa-regular fa-thumbs-up"></i></a>
+              <Icon class="icon" icon="line-md:thumbs-up-twotone" />
             </li>
             <!-- 踩 -->
             <li>
-              <a href="#"><i class="fa-regular fa-thumbs-down"></i></a>
+              <Icon class="icon" icon="line-md:thumbs-down-twotone" />
+            </li>
+            <li>
+              <Icon class="icon" icon="fa-regular:comment-dots" />
             </li>
           </ul>
         </div>
       </div>
       <!-- 单个评论内容区底部 -->
-      <div class="single-comment-content-bottom">
+      <div class="text">
         啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星
         啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星啊这可海星
       </div>
@@ -42,6 +48,7 @@
 /* 单个评论容器 */
 .container {
   display: flex;
+  width: 100%;
   margin: 10px 0;
 }
 .container img {
@@ -51,36 +58,37 @@
 }
 /* 单个评论的内容区 */
 .content {
+  width: 100%;
   display: flex;
   flex-direction: column;
 }
 /* 单个评论内容区顶部 */
-.single-comment-content-top {
+.describe {
   display: flex;
   justify-content: space-between;
 }
 /* 顶部左侧名字 */
-.single-comment-left {
+.name {
   font-size: 15px;
 }
-.single-comment-left a {
+.name a {
   color: $kungalgame-blue-5;
 }
-.single-comment-left a:hover {
+.name a:hover {
   text-decoration: underline;
 }
-.single-comment-right ul {
+.operate ul {
   display: flex;
   align-items: center;
 }
-.single-comment-right ul li {
+.operate ul li {
   margin-right: 10px;
 }
-.single-comment-right ul li a {
+.icon {
   color: $kungalgame-font-color-2;
 }
 /* 单个评论内容区底部 */
-.single-comment-content-bottom {
+.text {
   font-size: 12px;
   text-indent: 25px;
   /* 标题显示两行、超出部分隐藏 */
