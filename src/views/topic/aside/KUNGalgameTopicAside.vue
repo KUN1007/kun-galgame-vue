806807636a2ddf40dce8917cb2bc28c4540ade62
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-05-31 10:53:27 +0800

    optimized footer, topic aside item

--- src/views/topic/aside/KUNGalgameTopicAside.vue
+++ src/views/topic/aside/KUNGalgameTopicAside.vue
@@ -6,17 +6,18 @@
   页面被拆成 3 个大组件，这是左边的侧边栏部分
  -->
 <script setup lang="ts">
-import TopicAsideNav from './nav/TopicAsideNav.vue'
-import TopicAsideSame from './same/TopicAsideSame.vue'
-import TopicAsideMaster from './master/TopicAsideMaster.vue'
-import TopicAsideFooter from './footer/TopicAsideFooter.vue'
+import TopicAsideNav from './components/TopicAsideNav.vue'
+import TopicAsideSame from './components/TopicAsideSame.vue'
+import TopicAsideMaster from './components/TopicAsideMaster.vue'
+// 导入 Footer
+import KUNGalgameFooter from '@/components/KUNGalgameFooter.vue'
 </script>
 <template>
   <div class="content-left-part">
     <TopicAsideNav />
     <TopicAsideSame />
     <TopicAsideMaster />
-    <TopicAsideFooter />
+    <KUNGalgameFooter />
   </div>
 </template>
 <style scoped>
--- src/views/topic/content/components/Rewrite.vue
+++ src/views/topic/content/components/Rewrite.vue
@@ -6,7 +6,7 @@
   <span>在2019年10月7日10:07:00重新编辑</span>
 </template>
 
-<style lang="less" scoped>
+<style lang="scss" scoped>
 /* 内容区域下方字体样式 */
 span {
   margin: 0 10px;
