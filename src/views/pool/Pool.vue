3ae9e3203a05b4edb4140bfadc9e4ca4d433f567
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-06-12 14:26:03 +0800

    remove dependence vue-waterfall-plugin-next

--- src/views/pool/Pool.vue
+++ src/views/pool/Pool.vue
@@ -3,57 +3,6 @@ import KUNGalgameTopBar from '@/components/KUNGalgameTopBar.vue'
 import KUNGalgameSearchBox from '@/components/KUNGalgameSearchBox.vue'
 import Tags from './components/Tags.vue'
 import Topic from './components/Topic.vue'
-// 导入瀑布流
-import { Waterfall } from 'vue-waterfall-plugin-next'
-import 'vue-waterfall-plugin-next/dist/style.css'
-
-const list = [
-  {
-    index: 1,
-    image: '/src/assets/images/bg/bg1-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 2,
-    image: '/src/assets/images/bg/bg2-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 3,
-    image: '/src/assets/images/bg/bg3-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 4,
-    image: '/src/assets/images/bg/bg4-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 5,
-    image: '/src/assets/images/bg/bg5-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 6,
-    image: '/src/assets/images/bg/bg6-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 7,
-    image: '/src/assets/images/bg/bg7-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 8,
-    image: '/src/assets/images/bg/bg8-m.png',
-    alt: 'azkhx',
-  },
-  {
-    index: 9,
-    image: '/src/assets/images/bg/bg9-m.png',
-    alt: 'azkhx',
-  },
-]
 </script>
 
 <template>
@@ -68,22 +17,7 @@ const list = [
       <div class="content-container">
         <!-- 帖子池单个帖子列容器 -->
         <div class="single-topic-column-container">
-          <Waterfall
-            :list="list"
-            :background-color="'#00000000'"
-            :breakpoints="{
-              800: {
-                rowPerView: 4,
-              },
-              576: {
-                rowPerView: 3,
-              },
-            }"
-          >
-            <template #item="{ item, url, index }">
-              <Topic />
-            </template>
-          </Waterfall>
+          <Topic />
         </div>
       </div>
     </div>
