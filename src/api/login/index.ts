14c485055bddc2f4554d80b0b7a1a8a5b5fdeeaa
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-08-22 20:12:49 +0800

    feat: message component

--- src/api/login/index.ts
+++ src/api/login/index.ts
@@ -5,18 +5,9 @@ import type * as Login from './types/login'
 export const postLoginDataApi = async (
   loginData: Login.LoginRequestData
 ): Promise<Login.LoginResponseData> => {
-  try {
-    const response = await fetchPost<Login.LoginResponseData>(
-      '/login/login',
-      loginData
-    )
-    return response
-  } catch (error) {
-    alert(
-      '\nERROR! Please check your username, mail or password!\n\n错误!请检查您的用户名,邮箱或者密码'
-    )
-    throw new Error(
-      'Login Error! Please check your username, mail or password!'
-    )
-  }
+  const response = await fetchPost<Login.LoginResponseData>(
+    '/login/login',
+    loginData
+  )
+  return response
 }
