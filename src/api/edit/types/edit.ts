602db0fc1efecdaf70f0e3cec27b853c69938086
KUN1007 <yuyu_kun@sina.com>
2023-08-27 01:11:05 +0800

    mod: rebuild topic page

--- src/api/edit/types/edit.ts
+++ src/api/edit/types/edit.ts
@@ -9,24 +9,24 @@ export interface CreateTopicRequestData {
 
 export interface KUNGalgameTopic {
   tid: number
-  title: string
-  content: string
-  uid: number
-  tags: Array<string>
-  category: Array<string>
-  rid: Array<number>
-  time: string
-  popularity: number
-  upvotes: number
-  views: number
-  likes: number
-  replies: number
-  share: number
-  comments: number
-  dislikes: number
-  status: number
-  edited: number
+  // title: string
+  // content: string
+  // uid: number
+  // tags: Array<string>
+  // category: Array<string>
+  // rid: Array<number>
+  // time: string
+  // popularity: number
+  // upvotes: number
+  // views: number
+  // likes: number
+  // replies: number
+  // share: number
+  // comments: number
+  // dislikes: number
+  // status: number
+  // edited: number
 }
 
-// 登录响应数据的格式
+// 创建话题响应数据的格式
 export type CreateTopicResponseData = KUNGalgameResponseData<KUNGalgameTopic>
