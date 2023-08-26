b55b9a0181913115f6661ad3e356393e7cc0c509
KUN1007 <yuyu_kun@sina.com>
2023-08-26 19:59:03 +0800

    mod: floor and comment style

--- src/views/topic/content/others/components/Comments.vue
+++ src/views/topic/content/others/components/Comments.vue
@@ -6,39 +6,28 @@ import SingleComment from './SingleComment.vue'
 </script>
 
 <template>
-  <!-- 底部的评论 -->
+  <!-- 评论容器 -->
   <div class="comment">
     <!-- 评论的标题 -->
     <div class="title">
       <span>评论</span>
     </div>
-    <!-- 评论容器 -->
-    <div class="container">
-      <SingleComment />
-      <SingleComment />
-      <SingleComment />
-    </div>
+    <SingleComment />
+    <SingleComment />
+    <SingleComment />
   </div>
 </template>
 
-<style scoped>
-/* 底部的评论 */
-.comment {
-  display: flex;
-  align-items: center;
-  justify-content: flex-start;
-  flex-shrink: 0;
-  margin-top: 10px;
-}
+<style lang="scss" scoped>
 /* 评论的标题 */
 .title {
   flex-shrink: 0;
-  margin: 10px;
+  margin-bottom: 17px;
   color: var(--kungalgame-font-color-3);
 }
 /* 评论容器 */
-.container {
+.comment {
   width: 100%;
-  margin-bottom: 10px;
+  padding: 17px;
 }
 </style>
