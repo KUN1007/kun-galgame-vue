d139131e8947dab328c154cea7e062871955488e
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-09-01 22:33:45 +0800

    feat: editor frame

--- src/components/editor/KUNGalgameEditor.vue
+++ src/components/editor/KUNGalgameEditor.vue
@@ -1,11 +1,17 @@
-<script setup lang='ts'>
-
+<script setup lang="ts">
+import KUNGalgameEditorMenu from './KUNGalgameEditorMenu.vue'
 </script>
 
 <template>
-
+  <div class="editor-container">
+    <KUNGalgameEditorMenu />
+    <div class="editor"></div>
+  </div>
 </template>
 
-<style lang='scss' scoped>
-
+<style lang="scss" scoped>
+.editor-container {
+  min-height: 300px;
+  padding: 10px;
+}
 </style>
