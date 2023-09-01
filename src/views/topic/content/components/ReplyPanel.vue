1aa487bef86a4ca45adc5db445bcf7b8a05c18d6
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-09-01 20:09:09 +0800

    chore: remove unused dependence

--- src/views/topic/content/components/ReplyPanel.vue
+++ src/views/topic/content/components/ReplyPanel.vue
@@ -9,7 +9,7 @@ import 'animate.css'
 import { defineAsyncComponent } from 'vue'
 
 // 导入编辑器
-import Milkdown from '@/components/Milkdown/MilkdownProvider.vue'
+import KUNGalgameEditor from '@/components/editor/KUNGalgameEditor.vue'
 
 // 异步导入话题标签
 const Tags = defineAsyncComponent(
@@ -52,7 +52,7 @@ const handelClosePanel = () => {
           </div>
           <!-- 回复的编辑器 -->
           <div class="content">
-            <Milkdown />
+            <KUNGalgameEditor />
           </div>
           <!-- 回复的页脚 -->
           <div class="footer">
