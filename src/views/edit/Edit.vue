ee8f27cc27c32153b6341d3aaf2cbb409e2bcb6c
KUN1007 <yuyu_kun@sina.com>
2023-09-01 22:33:45 +0800

    feat: editor frame

--- src/views/edit/Edit.vue
+++ src/views/edit/Edit.vue
@@ -1,51 +1,9 @@
 <script setup lang="ts">
-import { onBeforeMount, ref } from 'vue'
 import KUNGalgameEditor from '@/components/editor/KUNGalgameEditor.vue'
 import Tags from './components/Tags.vue'
 import Footer from './components/Footer.vue'
+import Header from './components/Header.vue'
 import KUNGalgameFooter from '@/components/KUNGalgameFooter.vue'
-
-// 导入编辑帖子的 store
-import { useKUNGalgameEditStore } from '@/store/modules/edit'
-import { storeToRefs } from 'pinia'
-
-// 导入防抖函数
-import { debounce } from '@/utils/debounce'
-
-const topicData = storeToRefs(useKUNGalgameEditStore())
-
-// 话题标题的文字
-const topicTitle = ref('')
-// 标题的最大长度
-const maxInputLength = 40
-
-onBeforeMount(() => {
-  if (topicData.isSave.value) {
-    topicTitle.value = topicData.title.value
-  }
-})
-
-// 处理用户输入
-const handelInput = () => {
-  // 标题不能超过 40 字
-  if (topicTitle.value.length > maxInputLength) {
-    topicTitle.value = topicTitle.value.slice(0, maxInputLength)
-  }
-
-  // 用户输入了纯空格
-  if (topicTitle.value.trim() === '') {
-    return
-  }
-
-  // 创建一个防抖处理函数
-  const debouncedInput = debounce(() => {
-    // 过滤 xss
-    topicData.title.value = topicTitle.value
-  }, 300)
-
-  // 调用防抖处理函数，会在延迟时间内只执行一次更新操作
-  debouncedInput()
-}
 </script>
 
 <template>
@@ -54,43 +12,26 @@ const handelInput = () => {
     <div class="container">
       <!-- 内容区容器 -->
       <div class="content">
-        <!-- 内容区的头部 -->
-        <div class="header">
-          <!-- 话题的标题 -->
-          <div class="title">
-            <input
-              type="text"
-              placeholder="请输入话题的标题（40字以内）"
-              v-model="topicTitle"
-              @input="handelInput"
-              :maxlength="maxInputLength"
-            />
-          </div>
-        </div>
+        <!-- 标题 -->
+        <Header />
         <!-- 编辑器 -->
         <KUNGalgameEditor />
       </div>
 
       <!-- 内容区的底部 -->
       <div class="content-footer">
+        <!-- 话题的 Tag -->
         <Tags />
-
+        <!-- 话题的 Footer -->
         <Footer />
       </div>
     </div>
     <!-- 版权 -->
     <KUNGalgameFooter style="margin: 0 auto" />
-    <span style="margin: 0 auto; color: var(--kungalgame-font-color-3)"
-      >Editor powered by wangEditor</span
-    >
   </div>
 </template>
 
 <style lang="scss" scoped>
-.content {
-  margin: 0 auto;
-}
-
 .root {
   height: 100vh;
   min-height: 1200px;
@@ -110,39 +51,12 @@ const handelInput = () => {
   color: var(--kungalgame-font-color-3);
   border: 1px solid var(--kungalgame-blue-1);
   padding: 10px;
+  backdrop-filter: blur(5px);
 }
 
-/* 容器的顶部 */
-.header {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-}
-
-/* 话题的发布标题 */
-.title {
-  width: 100%;
-}
-
-/* 话题标题的输入框 */
-.title input {
-  color: var(--kungalgame-font-color-2);
-  /* 距离外轮廓的距离 */
-  padding: 7px;
-  /* 内边距盒子 */
-  box-sizing: border-box;
-  width: 100%;
-  /* 标题输入字体大小 */
-  font-size: 40px;
+.content {
+  margin: 0 auto;
   border: 1px solid var(--kungalgame-blue-4);
-  background-color: var(--kungalgame-white);
-  margin-bottom: 10px;
-}
-
-/* 标题输入框 focus 之后的样式 */
-.title input:focus {
-  box-shadow: 0px 0px 5px var(--kungalgame-blue-4);
 }
 
 .content-footer {
