e603f2702b8fc45d2204a6c249753935ab980f41
KUN1007 <yuyu_kun@sina.com>
2023-10-20 01:08:53 +0800

    rebuild: settings loli

--- src/utils/loli.ts
+++ src/utils/loli.ts
@@ -6,87 +6,89 @@ import loliData from '@/assets/images/ren/ren.json'
 /* 随机数 */
 import { randomNum } from './random'
 
-// 获取本地图片文件，注意这里不能用 @ 作为基础路径，只能用 ..
-const getAssetsFile = (url: number) => {
-  return new URL(`../assets/images/ren/${url}.png`, import.meta.url).href
-}
+export const getLoli = () => {
+  // 获取本地图片文件，注意这里不能用 @ 作为基础路径，只能用 ..
+  const getAssetsFile = (url: number) => {
+    return new URL(`../assets/images/ren/${url}.webp`, import.meta.url).href
+  }
 
-/* 随机汗水 ？ */
-// const randomSweat = randomNum(0, 1)
-/* 随机眉毛 */
-const randomBrow = randomNum(1, 18)
+  /* 随机汗水 ？ */
+  // const randomSweat = randomNum(0, 1)
+  /* 随机眉毛 */
+  const randomBrow = randomNum(1, 18)
 
-/* 随机眼睛 */
-const randomEye = randomNum(19, 36)
+  /* 随机眼睛 */
+  const randomEye = randomNum(19, 36)
 
-/* 随机嘴巴 */
-const randomMouth = randomNum(37, 56)
+  /* 随机嘴巴 */
+  const randomMouth = randomNum(37, 56)
 
-/* 随机腮红 */
-const randomFace = randomNum(57, 62)
+  /* 随机腮红 */
+  const randomFace = randomNum(57, 62)
 
-/* 随机衣服 */
-const randomSkirt = randomNum(63, 72)
+  /* 随机衣服 */
+  const randomSkirt = randomNum(63, 70)
 
-// 定义一个 loli 对象
-const loli = {
-  lass: loliData[randomSkirt],
-  eye: loliData[randomEye],
-  brow: loliData[randomBrow],
-  mouth: loliData[randomMouth],
-  face: loliData[randomFace],
-}
+  // 定义一个 loli 对象
+  const loli = {
+    lass: loliData[randomSkirt],
+    eye: loliData[randomEye],
+    brow: loliData[randomBrow],
+    mouth: loliData[randomMouth],
+    face: loliData[randomFace],
+  }
 
-// 身体定位
-const loliBodyLeft = loli.lass.left + 'px'
-const loliBodyTop = loli.lass.top + 'px'
+  // 身体定位
+  const loliBodyLeft = loli.lass.left + 'px'
+  const loliBodyTop = loli.lass.top + 'px'
 
-// 眼睛定位
-const loliEyeLeft = loli.eye.left + 'px'
-const loliEyeTop = loli.eye.top + 'px'
+  // 眼睛定位
+  const loliEyeLeft = loli.eye.left + 'px'
+  const loliEyeTop = loli.eye.top + 'px'
 
-// 眉毛定位
-const loliBrowLeft = loli.brow.left + 'px'
-const loliBrowTop = loli.brow.top + 'px'
+  // 眉毛定位
+  const loliBrowLeft = loli.brow.left + 'px'
+  const loliBrowTop = loli.brow.top + 'px'
 
-// 嘴巴定位
-const loliMouthLeft = loli.mouth.left + 'px'
-const loliMouthTop = loli.mouth.top + 'px'
+  // 嘴巴定位
+  const loliMouthLeft = loli.mouth.left + 'px'
+  const loliMouthTop = loli.mouth.top + 'px'
 
-// 腮红定位
-const loliFaceLeft = loli.face.left + 'px'
-const loliFaceTop = loli.face.top + 'px'
+  // 腮红定位
+  const loliFaceLeft = loli.face.left + 'px'
+  const loliFaceTop = loli.face.top + 'px'
 
-// 身体的图片资源链接
-const lass = getAssetsFile(loli.lass.layer_id)
+  // 身体的图片资源链接
+  const body = getAssetsFile(loli.lass.layer_id)
 
-// 眼睛的图片资源链接
-const eye = getAssetsFile(loli.eye.layer_id)
+  // 眼睛的图片资源链接
+  const eye = getAssetsFile(loli.eye.layer_id)
 
-// 眉毛的图片资源链接
-const brow = getAssetsFile(loli.brow.layer_id)
+  // 眉毛的图片资源链接
+  const brow = getAssetsFile(loli.brow.layer_id)
 
-// 嘴巴的图片资源链接
-const mouth = getAssetsFile(loli.mouth.layer_id)
+  // 嘴巴的图片资源链接
+  const mouth = getAssetsFile(loli.mouth.layer_id)
 
-// 腮红的图片资源链接
-const face = getAssetsFile(loli.face.layer_id)
+  // 腮红的图片资源链接
+  const face = getAssetsFile(loli.face.layer_id)
 
-// 导出模块
-export {
-  loliBodyLeft,
-  loliBodyTop,
-  loliEyeLeft,
-  loliEyeTop,
-  loliBrowLeft,
-  loliBrowTop,
-  loliMouthLeft,
-  loliMouthTop,
-  loliFaceLeft,
-  loliFaceTop,
-  lass,
-  eye,
-  brow,
-  mouth,
-  face,
+  // 导出模块
+  return {
+    loliBodyLeft,
+    loliBodyTop,
+    loliEyeLeft,
+    loliEyeTop,
+    loliBrowLeft,
+    loliBrowTop,
+    loliMouthLeft,
+    loliMouthTop,
+    loliFaceLeft,
+    loliFaceTop,
+    body,
+    eye,
+    brow,
+    mouth,
+    face,
+  }
 }
