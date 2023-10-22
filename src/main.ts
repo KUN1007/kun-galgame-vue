82d634649d47c9916d96e595abf5390fc2c8bcec
KUN1007 <yuyu_kun@sina.com>
2023-10-22 16:53:39 +0800

    chore: update dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,7 @@
-// core
-import { createApp } from 'vue'
-// APP
+import { createSSRApp } from 'vue'
 import App from './App.vue'
-/* 引入 vue-router */
-import router from './router'
-// 引入 i18n
-import i18n from '@/language/i18n'
-// 引入路由守卫
-import { setupRouterGuard } from '@/router/guard'
-// 引入 pinia
-import { setupPinia } from '@/store/index'
-// 引入样式
-import '@/styles/index.scss'
 
-// 获取 App 实例
-const app = createApp(App)
-
-// 使用路由守卫
-setupRouterGuard(router)
-// 使用 pinia
-setupPinia(app)
-
-app.use(router).use(i18n).mount('#app')
+export const createApp = () => {
+  const app = createSSRApp(App)
+  return { app }
+}
