0c89063c662be6dbd006915ebf0ac0adb024d05a
KUN1007 <yuyu_kun@sina.com>
2023-10-22 18:50:12 +0800

    mod: remove server render

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,24 @@
-import { createSSRApp } from 'vue'
+// core
+import { createApp } from 'vue'
+// APP
 import App from './App.vue'
+/* 引入 vue-router */
+import router from './router'
+// 引入 i18n
+import i18n from '@/language/i18n'
+// 引入路由守卫
+import { setupRouterGuard } from '@/router/guard'
+// 引入 pinia
+import { setupPinia } from '@/store/index'
+// 引入样式
+import '@/styles/index.scss'
 
-export const createApp = () => {
-  const app = createSSRApp(App)
-  return { app }
-}
+// 获取 App 实例
+const app = createApp(App)
+
+// 使用路由守卫
+setupRouterGuard(router)
+// 使用 pinia
+setupPinia(app)
+
+app.use(router).use(i18n).mount('#app')
