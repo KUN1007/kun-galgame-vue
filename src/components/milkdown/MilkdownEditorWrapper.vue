35a3bf64d5eaabb311c13f8fc769014edb0d5b25
KUN1007 <yuyu_kun@sina.com>
2023-10-26 23:01:25 +0800

    refactor: use MilkdownEditor instead vue-quill

---
+++ src/components/milkdown/MilkdownEditorWrapper.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { MilkdownProvider } from '@milkdown/vue'
+import MilkdownEditor from './MilkdownEditor.vue'
+</script>
+
+<!-- MilkdownEditorWrapper.vue -->
+<template>
+  <MilkdownProvider>
+    <MilkdownEditor />
+  </MilkdownProvider>
+</template>
