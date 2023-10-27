f752a978433f4ae608487357407e2a768d508d67
KUN1007 <yuyu_kun@sina.com>
2023-10-28 00:48:31 +0800

    feat: MilkdownEditor

--- src/components/milkdown/MilkdownEditor.vue
+++ src/components/milkdown/MilkdownEditor.vue
@@ -1,18 +1,240 @@
 <script setup lang="ts">
+// KUN Visual Novel Menu
+import MilkdownMenu from './plugins/MilkdownMenu.vue'
+// Milkdown core
 import { Editor, rootCtx, defaultValueCtx } from '@milkdown/core'
 import { Milkdown, useEditor } from '@milkdown/vue'
 import { commonmark } from '@milkdown/preset-commonmark'
+import { gfm } from '@milkdown/preset-gfm'
+// Milkdown Plugins
+import { history } from '@milkdown/plugin-history'
+import { prism, prismConfig } from '@milkdown/plugin-prism'
+import { listener, listenerCtx } from '@milkdown/plugin-listener'
+import { clipboard } from '@milkdown/plugin-clipboard'
+import { indent } from '@milkdown/plugin-indent'
+import { trailing } from '@milkdown/plugin-trailing'
+import { usePluginViewFactory } from '@prosemirror-adapter/vue'
+// KUN Visual Novel Custom tooltip
+import { tooltipFactory } from '@milkdown/plugin-tooltip'
+import Tooltip from './plugins/Tooltip.vue'
+// Custom text size calculate
+import Size from './plugins/Size.vue'
+import { $prose } from '@milkdown/utils'
+import { Plugin } from '@milkdown/prose/state'
 
-useEditor((root) =>
+// KUN Visual Novel style
+import '@/styles/editor/index.scss'
+
+// Syntax highlight
+import cpp from 'refractor/lang/cpp'
+import csharp from 'refractor/lang/csharp'
+import css from 'refractor/lang/css'
+import go from 'refractor/lang/go'
+import python from 'refractor/lang/python'
+import javascript from 'refractor/lang/javascript'
+import typescript from 'refractor/lang/typescript'
+import jsx from 'refractor/lang/jsx'
+import rust from 'refractor/lang/rust'
+import sql from 'refractor/lang/sql'
+import tsx from 'refractor/lang/tsx'
+import markdown from 'refractor/lang/markdown'
+
+// Editor markdown preset
+const valueMarkdown = `# Milkdown Vanilla Shiki Highlight
+
+> You're scared of a world where you're needed.
+
+\`\`\`rust
+Editor
+  .make()
+  .config(ctx => {
+    ctx.set(rootCtx, '#app')
+    ctx.set(defaultValueCtx, markdown)
+  })
+  .config(nord)
+  .use(commonmark)
+  .use(milkShiki)
+  .create()
+\`\`\`
+
+This is a demo for using Milkdown with **Vanilla Typescript**.
+The code block is highlighted by [shiki](https://shiki.matsu.io/).`
+
+const tooltip = tooltipFactory('Text')
+const pluginViewFactory = usePluginViewFactory()
+
+const editorInfo = useEditor((root) =>
   Editor.make()
     .config((ctx) => {
       ctx.set(rootCtx, root)
+      ctx.set(defaultValueCtx, valueMarkdown)
+
+      const listener = ctx.get(listenerCtx)
+
+      listener.markdownUpdated((ctx, markdown, prevMarkdown) => {
+        if (markdown !== prevMarkdown) {
+        }
+      })
+
+      ctx.set(prismConfig.key, {
+        configureRefractor: (refractor) => {
+          refractor.register(cpp)
+          refractor.register(csharp)
+          refractor.register(css)
+          refractor.register(go)
+          refractor.register(python)
+          refractor.register(markdown)
+          refractor.register(javascript)
+          refractor.register(typescript)
+          refractor.register(jsx)
+          refractor.register(rust)
+          refractor.register(sql)
+          refractor.register(tsx)
+        },
+      })
+
+      ctx.set(tooltip.key, {
+        view: pluginViewFactory({
+          component: Tooltip,
+        }),
+      })
     })
+    .use(history)
     .use(commonmark)
+    .use(gfm)
+    .use(prism)
+    .use(listener)
+    .use(clipboard)
+    .use(indent)
+    .use(trailing)
+    .use(tooltip)
+    // Add custom plugin view
+    .use(
+      $prose(
+        () =>
+          new Plugin({
+            view: pluginViewFactory({
+              component: Size,
+            }),
+          })
+      )
+    )
 )
 </script>
 
 <!-- MilkdownEditor.vue -->
 <template>
-  <Milkdown />
+  <div class="editor-container">
+    <MilkdownMenu :editorInfo="editorInfo" />
+    <Milkdown class="editor" />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.editor {
+  :deep(.milkdown) {
+    width: 100%;
+    padding: 10px;
+
+    /* Silence css check */
+    * {
+      white-space: pre-wrap;
+    }
+
+    & > div:nth-child(1) {
+      margin: 0 auto;
+      min-height: 300px;
+      overflow-y: scroll;
+
+      &::-webkit-scrollbar {
+        display: inline;
+        width: 7px;
+        height: 0;
+      }
+
+      &::-webkit-scrollbar-thumb {
+        cursor: default;
+        background: var(--kungalgame-blue-4);
+        border-radius: 3px;
+      }
+
+      /* Compatible with Firefox */
+      scrollbar-width: thin;
+      scrollbar-color: var(--kungalgame-blue-4) var(--kungalgame-blue-1); /* Firefox 64+ */
+    }
+
+    del {
+      text-decoration: line-through;
+    }
+
+    p {
+      margin: 17px 0;
+    }
+
+    blockquote {
+      margin: 17px 0;
+      padding: 10px;
+      font-size: 18px;
+      border-left: 4px solid var(--kungalgame-blue-4);
+      background-color: var(--kungalgame-trans-blue-0);
+
+      p {
+        margin: 0;
+      }
+    }
+
+    pre {
+      margin: 17px 0;
+      border: 1px solid var(--kungalgame-blue-4);
+      border-radius: 5px;
+      padding: 17px;
+      background-color: var(--kungalgame-trans-white-2);
+      position: relative;
+
+      code {
+        font-size: 15px;
+        font-family: monospace;
+      }
+    }
+
+    a {
+      cursor: pointer;
+      font-style: oblique;
+      font-weight: bold;
+      color: var(--kungalgame-blue-4);
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    table {
+      border: 1px solid var(--kungalgame-blue-4);
+      border-radius: 5px;
+      white-space: nowrap;
+    }
+
+    th,
+    td {
+      border: 1px solid var(--kungalgame-blue-4);
+      padding: 3px;
+      text-align: left;
+    }
+
+    tr:nth-child(even) {
+      background-color: var(--kungalgame-trans-blue-1);
+    }
+
+    ul li,
+    ol li {
+      color: var(--kungalgame-blue-4);
+    }
+
+    .tableWrapper {
+      color: var(--kungalgame-font-color-3);
+      position: relative;
+      overflow-x: auto;
+    }
+  }
+}
+</style>
--- src/components/milkdown/MilkdownEditorWrapper.vue
+++ src/components/milkdown/MilkdownEditorWrapper.vue
@@ -1,11 +1,24 @@
 <script setup lang="ts">
 import { MilkdownProvider } from '@milkdown/vue'
+import { ProsemirrorAdapterProvider } from '@prosemirror-adapter/vue'
 import MilkdownEditor from './MilkdownEditor.vue'
 </script>
 
 <!-- MilkdownEditorWrapper.vue -->
 <template>
-  <MilkdownProvider>
-    <MilkdownEditor />
-  </MilkdownProvider>
+  <div class="editor">
+    <MilkdownProvider>
+      <ProsemirrorAdapterProvider>
+        <MilkdownEditor />
+      </ProsemirrorAdapterProvider>
+    </MilkdownProvider>
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.editor {
+  margin: 10px;
+  border: 1px solid var(--kungalgame-blue-4);
+  border-radius: 5px;
+}
+</style>
