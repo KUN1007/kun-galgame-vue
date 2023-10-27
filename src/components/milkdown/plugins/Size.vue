8645dd231ca8e7cf117fd8895a03a6ef7e304242
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-10-28 00:48:31 +0800

    feat: MilkdownEditor

---
+++ src/components/milkdown/plugins/Size.vue
@@ -0,0 +1,18 @@
+<!-- Custom plugins, calculate text size -->
+
+<script setup lang="ts">
+import { usePluginViewContext } from '@prosemirror-adapter/vue'
+import { computed } from 'vue'
+
+const { view } = usePluginViewContext()
+
+const size = computed(() => {
+  return view.value.state.doc.textContent.length
+})
+</script>
+
+<template>
+  <div>
+    <span> Text count: {{ size }} </span>
+  </div>
+</template>
