9527ba74127d1202e1209d8f8cbbf072c4a09ee3
KUN1007 <yuyuyukunkunkun@gmail.com>
2023-11-28 23:20:34 +0800

    pref: code highlight

--- src/components/milkdown/MilkdownEditor.vue
+++ src/components/milkdown/MilkdownEditor.vue
@@ -27,15 +27,21 @@ import { Plugin } from '@milkdown/prose/state'
 import '@/styles/editor/index.scss'
 
 // Syntax highlight
+import c from 'refractor/lang/c'
 import cpp from 'refractor/lang/cpp'
 import csharp from 'refractor/lang/csharp'
 import css from 'refractor/lang/css'
 import go from 'refractor/lang/go'
+import haskell from 'refractor/lang/haskell'
 import python from 'refractor/lang/python'
+import java from 'refractor/lang/java'
 import javascript from 'refractor/lang/javascript'
 import typescript from 'refractor/lang/typescript'
 import jsx from 'refractor/lang/jsx'
+import kotlin from 'refractor/lang/kotlin'
+import r from 'refractor/lang/r'
 import rust from 'refractor/lang/rust'
+import scala from 'refractor/lang/scala'
 import sql from 'refractor/lang/sql'
 import tsx from 'refractor/lang/tsx'
 import markdown from 'refractor/lang/markdown'
@@ -86,16 +92,22 @@ const editorInfo = useEditor((root) =>
 
       ctx.set(prismConfig.key, {
         configureRefractor: (refractor) => {
+          refractor.register(c)
           refractor.register(cpp)
           refractor.register(csharp)
           refractor.register(css)
           refractor.register(go)
+          refractor.register(haskell)
           refractor.register(python)
           refractor.register(markdown)
+          refractor.register(java)
           refractor.register(javascript)
           refractor.register(typescript)
           refractor.register(jsx)
+          refractor.register(kotlin)
+          refractor.register(r)
           refractor.register(rust)
+          refractor.register(scala)
           refractor.register(sql)
           refractor.register(tsx)
         },
